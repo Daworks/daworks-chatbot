cb8b2507dd49a144858ac3e5f54dcfa8af1e4f52
Daworks <arete75@gmail.com>
2017-11-14 14:44:55 +0900

    show page 추가

--- public/partials/daworks-kakao-chatbot-public-display.php
+++ public/partials/daworks-kakao-chatbot-public-display.php
@@ -3,7 +3,7 @@
 		if ( count ( $records ) == 0 ) :
 			echo '<p class="text-center">등록된 스토리가 없습니다.</p>';
 		else :
-			echo '<p class="text-center">'.$total.'개의 글이 등록되었습니다</p>';
+			echo '<p class="text-center">&quot;'.$total.'개&quot;의 스토리가 등록되었습니다</p>';
 		endif;
 	?>
 </div>
@@ -11,27 +11,29 @@
 	<?php
 		foreach ( $records as $card )  :
 	?>
-		 <div class="card">
-				 <?php if ( $card -> photo_check == 1 ) : ?>
-				<picture>
-					<a href="<?php echo $card -> photo ?>" data-lightbox="daworks-chatbot-show">
-						<img src="<?php echo $card -> photo ?>" alt="" class="img-responsive">
-					</a>
-				</picture>
-				 <?php endif ?>
+		 <div class="card" onclick="location.href='<?php printf('%s?mode=show&show_id=%s&current_page=%s', get_permalink(), $card->id, $page) ?>'">
+			 <?php
+				$now = new DateTime("now", new DateTimeZone("Asia/Seoul"));
+				$regist_day = new DateTime($card->created_at, new DateTimeZone("Asia/Seoul"));
+				$diff = $now->diff($regist_day);
+				if ( $diff->days < 2 ) :
+			 ?>
+			   <span class="badge">NEW</span>
+			 <?php endif ?>
+				<picture style="background-image:url(<?php echo ($card -> photo_check==1) ? $card -> photo : plugin_dir_url(__DIR__) . '../library/img/noimg.jpg'; ?>)"></picture>
 			 <div class="card-title">
-						<?php echo $card -> title ?>
+				<?php echo $card -> title ?>
 			 </div>
 			 <div class="card-content">
-						<?php echo $card -> story ?>
+				<?php echo $card -> story ?>
 			 </div>
 			 <div class="card-name">
-						<?php echo $card -> name ?>
+				 <?php echo $card -> name ?>
 				 <span class="card-date">
 					 <?php
-								  $dt = new DateTime( $card -> created_at );
-								  echo $dt -> format ( 'Y-m-d H:i' );
-							  ?>
+						  $dt = new DateTime( $card -> created_at, new DateTimeZone("Asia/Seoul") );
+						  echo $dt -> format ( 'Y-m-d H:i' );
+					  ?>
 			</span>
 			 </div>
 		 </div>
---
+++ public/partials/daworks-kakao-chatbot-public-show.php
@@ -0,0 +1,46 @@
+<script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+<section>
+	<div id="daworks-chatbot-show">
+		<div class="entry-box">
+			<h3 class="entry-title">
+				&quot;<?php echo $card -> title ?>&quot;
+			</h3>
+			<div class="entry-meta">
+				<span class="writer"><attr><i data-feather="user"></i><?php echo $card -> name ?></attr></span> 님이</span> <span class="created_at"><?php echo $card -> created_at ?></span>에 작성하신 글입니다.
+			</div>
+		</div>
+		<div class="entry-content">
+				<?php if ( $card -> photo_check == 1 ) : ?>
+			  <picture>
+				  <img src="<?= $card -> photo ?>" alt="">
+			  </picture>
+				<? endif ?>
+			<div class="content-body">
+					 <?php echo nl2br($card -> story) ?>
+			</div>
+			
+			<table class="daworks_chatbot_navigation">
+				<tbody>
+				<tr>
+					<td class="text-left">
+					<?php if ( $next_id ) : ?>
+						<a href="<?php printf("%s?mode=show&show_id=%s&current_page=%s", get_permalink(), $next_id, sanitize_text_field ( $_REQUEST['current_page'])); ?>"><i data-feather="arrow-left"></i> <?php echo mb_strimwidth($next_title, 0, 30, '...') ?></a>
+					<?php endif; ?>
+					</td>
+					<td class="text-center">
+						<a href="<?php printf("%s?mode=list&current_page=%s", get_permalink(), sanitize_text_field ( $_REQUEST['current_page'])); ?>"><i data-feather="grid"></i></a>
+					</td>
+					<td class="text-right">
+					<?php if ( $prev_id ) : ?>
+						<a href="<?php printf("%s?mode=show&show_id=%s&current_page=%s", get_permalink(), $prev_id, sanitize_text_field ( $_REQUEST['current_page'])); ?>"><?php echo mb_strimwidth($prev_title, 0, 30, '...') ?> <i data-feather="arrow-right"></i></a>
+					<?php endif ?>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</section>
+<script>
+	feather.replace()
+</script>
