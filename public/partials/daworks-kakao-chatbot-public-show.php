bd0274a2cbffe9fa8ec82cf17964e33552df8170
Design Arete <arete75@gmail.com>
2018-01-26 06:32:44 +0900

    Update daworks-kakao-chatbot-public-show.php

--- public/partials/daworks-kakao-chatbot-public-show.php
+++ public/partials/daworks-kakao-chatbot-public-show.php
@@ -1,3 +1,11 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.11&appId=1617805988472516&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
 <section>
 	<div id="daworks-chatbot-show">
