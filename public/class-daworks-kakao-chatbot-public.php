80f127a160a7293a278573de9d5c32f0df827246
Daworks <arete75@gmail.com>
2019-07-30 16:41:03 +0900

    public show mode 수정

--- public/class-daworks-kakao-chatbot-public.php
+++ public/class-daworks-kakao-chatbot-public.php
@@ -80,7 +80,7 @@
 			
 			add_shortcode ( 'daworks-chatbot-view', 'show_frontend_page' );
 			function show_frontend_page () {
-			    ob_start();
+
 				$host = $_SERVER['HTTP_HOST'];
 				if ( $host === 'dev.daworks.io' ) :
 				?>
@@ -171,8 +171,6 @@
 				
 				print('<p class="daworks-copyright"><a href="https://daworks.io">&copy;2017 디자인아레테</a></p>');
 
-				ob_end_flush();
-				
 			}
 		}
 		
